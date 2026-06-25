d329e9830f53f6d4d72b2474304e92c3e70eb985
thig0S_ <thiagokovalski5@gmail.com>
2026-06-25 13:20:05 -0300

    Finalizada classe Contato

--- EAgendaWeb.WebApp/Modulos/ModuloContato/Dominio/Contato.cs
+++ EAgendaWeb.WebApp/Modulos/ModuloContato/Dominio/Contato.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using EAgendaWeb.WebApp.Compartilhado.Dominio;
 
 namespace EAgendaWeb.WebApp.Modulos.ModuloContato.Dominio;
@@ -12,7 +13,7 @@ public class Contato : EntidadeBase<Contato>
 
     public Contato()
     {
-        
+
     }
 
     public Contato(string nome, string email, string telefone, string cargo, string empresa)
@@ -24,14 +25,53 @@ public class Contato : EntidadeBase<Contato>
         Empresa = empresa;
     }
 
-
     public override void Atualizar(Contato entidadeAtualizada)
     {
-        throw new NotImplementedException();
+        Nome = entidadeAtualizada.Nome;
+        Email = entidadeAtualizada.Email;
+        Telefone = entidadeAtualizada.Telefone;
+        Cargo = entidadeAtualizada.Cargo;
+        Empresa = entidadeAtualizada.Empresa;
     }
 
     public override List<string> Validar()
     {
-        throw new NotImplementedException();
+        List<string> erros = [];
+
+        if (Nome.Length < 2 && Nome.Length > 100)
+            erros.Add("O campo \"Nome\" deve conter entre 2 a 100 caracteres");
+
+        bool isValid = new EmailAddressAttribute().IsValid(Email);
+
+        if (!isValid)
+            erros.Add("O campo \"Email\" deve conter um Email válido!");
+
+        if (Email.Length < 2 && Email.Length > 100)
+            erros.Add("O campo \"Nome\" deve conter entre 2 a 100 caracteres");
+
+        string telefoneEncurtado = Telefone.Replace(" ", "").Replace("-", "").Replace("(", "").Replace(")", "");
+        bool contemLetraOuSimbolo = false;
+
+        int contadorDigitos = 0;
+
+        for (int i = 0; i < telefoneEncurtado.Length; i++)
+        {
+            char c = telefoneEncurtado[i];
+            if (char.IsDigit(c))
+                contadorDigitos++;
+            else
+            {
+                contemLetraOuSimbolo = true;
+                break;
+            }
+        }
+
+        if (contadorDigitos < 10 || contadorDigitos > 11)
+            erros.Add("O campo \"Telefone\" deve conter entre 10 e 11 dígitos;");
+
+        if (contemLetraOuSimbolo)
+            erros.Add("O campo \"Telefone\" deve conter apenas dígitos;");
+
+        return erros;
     }
 }
