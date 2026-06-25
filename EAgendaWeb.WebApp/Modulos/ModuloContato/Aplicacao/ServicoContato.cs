1779a1155b5182c2713685f48c8a6134ec17cac3
thig0S_ <thiagokovalski5@gmail.com>
2026-06-25 15:37:52 -0300

    Adicionado validacao de emails e telefones repetidos

--- EAgendaWeb.WebApp/Modulos/ModuloContato/Aplicacao/ServicoContato.cs
+++ EAgendaWeb.WebApp/Modulos/ModuloContato/Aplicacao/ServicoContato.cs
@@ -31,6 +31,12 @@ public class ServicoContato
         if (resultadoValidacao.IsFailed)
             return resultadoValidacao;
 
+        if (ExisteContatoComMesmoTelefone(novoContato.Telefone))
+            return Falha(nameof(dto.Telefone), "Já existe um Contato com este Telefone.");
+
+        if (ExiteContatoComMesmoEmail(novoContato.Email))
+            return Falha(nameof(dto.Email), "Já existe um Contato com este Email.");
+
         repositorioContato.Cadastrar(novoContato);
 
         return Result.Ok();
@@ -43,5 +49,22 @@ public class ServicoContato
             return Result.Ok();
 
         return Result.Fail(new FluentResults.Error(erros.First()).WithMetadata("Campo", string.Empty));
+    }
+
+    private bool ExisteContatoComMesmoTelefone(string telefone, Guid? idIgnorado = null)
+    {
+        return repositorioContato.SelecionarTodos().Any(c => c.Id != idIgnorado &&
+        string.Equals(c.Telefone, telefone, StringComparison.OrdinalIgnoreCase)
+        );
+    }
+    private bool ExiteContatoComMesmoEmail(string email, Guid? idIgnorado = null)
+    {
+        return repositorioContato.SelecionarTodos().Any(c => c.Id != idIgnorado &&
+        string.Equals(c.Email, email, StringComparison.OrdinalIgnoreCase)
+        );
+    }
+    private static Result Falha(string campo, string mensagem)
+    {
+        return Result.Fail(new FluentResults.Error(mensagem).WithMetadata("Campo", campo));
     }
 }
