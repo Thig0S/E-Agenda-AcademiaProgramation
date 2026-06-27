b5c55ce2815434c13486572a2821a3e2ace632a2
Davi Andrade <contatodaviandrade20@gmail.com>
2026-06-26 21:21:52 -0300

    Adicionada View Listar

--- EAgendaWeb.WebApp/Modulos/ModuloCategoria/Apresentacao/CategoriaController.cs
+++ EAgendaWeb.WebApp/Modulos/ModuloCategoria/Apresentacao/CategoriaController.cs
@@ -1,11 +1,27 @@
+using EAgendaWeb.WebApp.Modulos.ModuloCategoria.Dominio;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EAgendaWeb.WebApp.Modulos.ModuloCategoria.Apresentacao;
 
 public class CategoriaController : Controller
 {
+    private readonly IRepositorioCategoria repositorioCategoria;
+
+    public CategoriaController(IRepositorioCategoria repositorioCategoria)
+    {
+        this.repositorioCategoria = repositorioCategoria;
+    }
+
     public ActionResult Listar()
     {
-        return View();
+        List<Categoria> listaCategorias = repositorioCategoria.SelecionarTodos();
+        List<ListarCategoriaViewModel> listarVms = [];
+        foreach (var item in listaCategorias)
+        {
+            ListarCategoriaViewModel vm = new(item.Id.ToString(), item.Titulo);
+
+            listarVms.Add(vm);
+        }
+        return View(listarVms);
     }
 }
--- EAgendaWeb.WebApp/Modulos/ModuloCategoria/Dominio/Categoria.cs
+++ EAgendaWeb.WebApp/Modulos/ModuloCategoria/Dominio/Categoria.cs
@@ -6,6 +6,10 @@ public class Categoria : EntidadeBase<Categoria>
 {
     public string Titulo { get; set; }
 
+    public Categoria()
+    {
+
+    }
     public Categoria(string titulo)
     {
         Titulo = titulo;
