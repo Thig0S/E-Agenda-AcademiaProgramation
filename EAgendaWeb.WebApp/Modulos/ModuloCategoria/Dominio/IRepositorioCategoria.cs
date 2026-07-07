ca85ef9075e97c23a8b2881b7829ff648f70993f
thig0S_ <thiagokovalski5@gmail.com>
2026-07-07 14:02:26 -0300

    Base dos Projetos

--- EAgendaWeb.WebApp/Modulos/ModuloCategoria/Dominio/IRepositorioCategoria.cs
+++
@@ -1,5 +0,0 @@
-using EAgendaWeb.WebApp.Compartilhado.Dominio;
-
-namespace EAgendaWeb.WebApp.Modulos.ModuloCategoria.Dominio;
-
-public interface IRepositorioCategoria : IRepositorio<Categoria>;
--- EAgendaWeb.WebApp/Modulos/ModuloContato/Dominio/IRepositorioContato.cs
+++
@@ -1,5 +0,0 @@
-using EAgendaWeb.WebApp.Compartilhado.Dominio;
-
-namespace EAgendaWeb.WebApp.Modulos.ModuloContato.Dominio;
-
-public interface IRepositorioContato : IRepositorio<Contato>;
