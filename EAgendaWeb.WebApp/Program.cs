ed6913d0c2667466124482e275e2041db63662a0
thig0S_ <thiagokovalski5@gmail.com>
2026-07-07 18:55:20 -0300

    Adicionado Versão de desenvolvimento e versão de Produção - banco de dados local e azure

--- EAgendaWeb.WebApp/Program.cs
+++ EAgendaWeb.WebApp/Program.cs
@@ -4,6 +4,10 @@ using EAgendaWeb.WebApp.Compartilhado.Infra;
 
 var builder = WebApplication.CreateBuilder(args);
 
+if (builder.Environment.IsProduction())
+{
+    builder.Configuration.AddUserSecrets<Program>();
+}
 // Configuração de Dependências (Dependency Injection)
 builder.Services.AddInfraRepositories();
 
